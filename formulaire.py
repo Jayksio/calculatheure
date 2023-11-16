f23cc38b0c06640959c064ebc17a8fecfb35b820
Pierre Leroy <pleroy0307@gmail.com>
2023-11-16 13:09:32 +0100

    outil tarif 10mw, V1 non stylisée

--- formulaire.py
+++ formulaire.py
@@ -1,10 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
-import tarif  # Importation du module tarif
-
-
-noms = ["Calypso", "Alexandre", "Pierre", "Sophie", "Jacques", "Guillaume"]
-projets = ["AJP", "Colombbus", "10MW", "Renaissance"]
+import tarif
 
 def soumettre():
     nom = nom_var.get()
@@ -12,26 +8,61 @@ def soumettre():
     heures = heures_var.get()
     tarif.ajouter_donnees(nom, projet, heures)
 
+def afficher_cout_total():
+    projet = projet_var.get()
+    total = tarif.calculer_cout_total_projet(projet)
+    label_resultat.config(text=f"Coût total pour {projet}: {total}€")
+
+def afficher_cout_personne_projet_mois():
+    nom = nom_var.get()
+    projet = projet_var.get()
+    mois = int(mois_var.get())
+    annee = int(annee_var.get())
+    heures, cout = tarif.calculer_cout_et_heures_par_personne_projet_et_mois(nom, projet, mois, annee)
+    label_resultat.config(text=f"{nom} a travaillé {heures} heures sur {projet} en {mois}/{annee}. Coût total: {cout}€")
+
+# fenêtre principale
 fenetre = tk.Tk()
 fenetre.title("Formulaire de saisie des heures")
 
+# variables tkinter
 nom_var = tk.StringVar()
 projet_var = tk.StringVar()
 heures_var = tk.StringVar()
+mois_var = tk.StringVar()
+annee_var = tk.StringVar()
 
 ttk.Label(fenetre, text="Choisissez un nom :").grid(column=0, row=0)
-nom_menu = ttk.Combobox(fenetre, textvariable=nom_var, values=noms)
+nom_menu = ttk.Combobox(fenetre, textvariable=nom_var, values=["Calypso", "Alexandre", "Pierre", "Sophie", "Jacques", "Guillaume"])
 nom_menu.grid(column=1, row=0)
 
 ttk.Label(fenetre, text="Choisissez un projet :").grid(column=0, row=1)
-projet_menu = ttk.Combobox(fenetre, textvariable=projet_var, values=projets)
+projet_menu = ttk.Combobox(fenetre, textvariable=projet_var, values=["AJP", "Colombbus", "10MW", "Renaissance"])
 projet_menu.grid(column=1, row=1)
 
 ttk.Label(fenetre, text="Heures travaillées :").grid(column=0, row=2)
 heures_entree = ttk.Entry(fenetre, textvariable=heures_var)
 heures_entree.grid(column=1, row=2)
 
-bouton_soumettre = ttk.Button(fenetre, text="Soumettre", command=soumettre)
-bouton_soumettre.grid(column=1, row=3)
+ttk.Label(fenetre, text="Mois (MM):").grid(column=0, row=3)
+mois_entree = ttk.Entry(fenetre, textvariable=mois_var)
+mois_entree.grid(column=1, row=3)
 
+ttk.Label(fenetre, text="Année (AAAA):").grid(column=0, row=4)
+annee_entree = ttk.Entry(fenetre, textvariable=annee_var)
+annee_entree.grid(column=1, row=4)
+
+bouton_soumettre = ttk.Button(fenetre, text="Soumettre", command=soumettre)
+bouton_soumettre.grid(column=1, row=5)
+
+bouton_cout_total = ttk.Button(fenetre, text="Calculer le coût total du projet", command=afficher_cout_total)
+bouton_cout_total.grid(column=1, row=6)
+
+bouton_cout_personne_projet_mois = ttk.Button(fenetre, text="Calculer par personne, mois et projet", command=afficher_cout_personne_projet_mois)
+bouton_cout_personne_projet_mois.grid(column=1, row=7)
+
+label_resultat = ttk.Label(fenetre, text="")
+label_resultat.grid(column=1, row=8)
+
+# interface utilisateur
 fenetre.mainloop()
--- tarif.py
+++ tarif.py
@@ -1,10 +1,45 @@
 import csv
+from datetime import datetime
 
-# Dictionnaire des taux horaires pour chaque personne
-taux_horaires = {"Calypso": 16.48, "Alexandre": 11.52, "Pierre": 11.52, "Sophie": 11.52, "Jacques": 11.52, "Guillaume": 11.52,}
+# les taux horaires de l'équipe
+taux_horaires = {"Calypso": 16.48, "Alexandre": 11.52, "Pierre": 11.52, "Sophie": 11.52, "Jacques": 11.52, "Guillaume": 11.52}
 
 def ajouter_donnees(nom, projet, heures):
+    date_actuelle = datetime.now().strftime("%Y-%m-%d")  # format AAAA-MM-JJ, à modifier si besoin mais j'ai utilisé ce format pour faciliter le tri
     cout = taux_horaires[nom] * int(heures)
     with open('donnees.csv', 'a', newline='') as fichier:
         ecrivain = csv.writer(fichier)
-        ecrivain.writerow([nom, projet, heures, cout])
+        ecrivain.writerow([nom, projet, heures, cout, date_actuelle])
+
+def calculer_cout_total_projet(nom_projet):
+    total = 0
+    with open('donnees.csv', 'r', newline='') as fichier:
+        lecteur = csv.reader(fichier)
+        for ligne in lecteur:
+            if ligne[1] == nom_projet:
+                total += float(ligne[3])
+    return round(total, 2)
+
+def calculer_cout_et_heures_par_personne_et_projet(nom_personne, nom_projet):
+    total_heures = 0
+    total_cout = 0
+    with open('donnees.csv', 'r', newline='') as fichier:
+        lecteur = csv.reader(fichier)
+        for ligne in lecteur:
+            if ligne[0] == nom_personne and ligne[1] == nom_projet:
+                total_heures += int(ligne[2])
+                total_cout += float(ligne[3])
+    return total_heures, round(total_cout, 2)
+
+def calculer_cout_et_heures_par_personne_projet_et_mois(nom_personne, nom_projet, mois, annee):
+    total_heures = 0
+    total_cout = 0
+    with open('donnees.csv', 'r', newline='') as fichier:
+        lecteur = csv.reader(fichier)
+        for ligne in lecteur:
+            date = datetime.strptime(ligne[4], "%Y-%m-%d")
+            if (ligne[0] == nom_personne and ligne[1] == nom_projet and 
+                date.month == mois and date.year == annee):
+                total_heures += int(ligne[2])
+                total_cout += float(ligne[3])
+    return total_heures, total_cout
